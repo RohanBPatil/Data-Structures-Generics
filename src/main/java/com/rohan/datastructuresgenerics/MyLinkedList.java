c6d4c9debde63b8ee6c78133cd3faf1a3edcd97e
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-12 23:18:14 +0530

    Modified : Add method to add new node at tail

--- src/main/java/com/rohan/datastructuresgenerics/MyLinkedList.java
+++ src/main/java/com/rohan/datastructuresgenerics/MyLinkedList.java
@@ -10,13 +10,13 @@ public class MyLinkedList {
 	}
 
 	public void add(INode newNode) {
-		if(tail == null)
+		if (head == null)
+			head = newNode;
+		if (tail == null)
 			tail = newNode;
-		if(head == null)
-			head = newNode;
 		else {
-			newNode.setNext(this.head);
-			head = newNode;
+			tail.setNext(newNode);
+			tail = newNode;
 		}
 	}
 }
--- src/test/java/com/rohan/datastructuresgenerics/MyLinkedListTest.java
+++ src/test/java/com/rohan/datastructuresgenerics/MyLinkedListTest.java
@@ -8,16 +8,15 @@ class MyLinkedListTest {
 
 	@Test
 	public void newNodeAdded_shouldReturn_newNode() {
-		MyNode<Integer> firstNode = new MyNode<>(70);
+		MyNode<Integer> firstNode = new MyNode<>(56);
 		MyNode<Integer> secondNode = new MyNode<>(30);
-		MyNode<Integer> thirdNode = new MyNode<>(56);
+		MyNode<Integer> thirdNode = new MyNode<>(70);
 		MyLinkedList myLinkedList = new MyLinkedList();
 		myLinkedList.add(firstNode);
 		myLinkedList.add(secondNode);
 		myLinkedList.add(thirdNode);
-
-		boolean result = myLinkedList.head.equals(thirdNode) && myLinkedList.head.getNext().equals(secondNode)
-				&& myLinkedList.head.getNext().equals(firstNode);
+		boolean result = myLinkedList.head.equals(firstNode) && myLinkedList.head.getNext().equals(secondNode)
+				&& myLinkedList.tail.equals(thirdNode);
 		assertTrue(result);
 	}
 
